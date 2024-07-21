2592376f9cd575b72cdbc5b3c643db4dbabd3376
BeyronssDev <josepbaroroca@gmail.com>
2024-07-21 13:48:14 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#excusa").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronombre = ["uno", "el"];
+  let sujeto = ["corredor", "mapache", "perro", "conductor", "comico", "piña"];
+  let accion = ["tomó mi", "tiro mi", "le grito a mi", "robó mi", "mordió mi"];
+  let posesion = ["trabajo", "dedo del pie", "coche", "zapato"];
+  let donde = ["en la calle", "en mi casa", "de camino a la entrada"];
+
+  let proIndex = Math.floor(Math.random() * pronombre.length);
+  let sujIndex = Math.floor(Math.random() * sujeto.length);
+  let accIndex = Math.floor(Math.random() * accion.length);
+  let posIndex = Math.floor(Math.random() * posesion.length);
+  let donIndex = Math.floor(Math.random() * donde.length);
+
+  return (
+    pronombre[proIndex] +
+    " " +
+    sujeto[sujIndex] +
+    " " +
+    accion[accIndex] +
+    " " +
+    posesion[posIndex] +
+    " " +
+    donde[donIndex]
+  );
 };
